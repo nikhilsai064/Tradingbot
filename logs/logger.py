aa50bd02c192a2530264f7e9e82c1fbdcecf8b04
nikhilsai064 <nikhilsai064@gmail.com>
2026-02-27 11:49:06 -0600

    print was added

--- logs/logger.py
+++ logs/logger.py
@@ -2,16 +2,19 @@ import logging
 import os
 from datetime import datetime
 
+# Create 'logs/' directory if it doesn't exist
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Create unique log file name
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
-logs_path = os.path.join(os.getcwd(),"logs", LOG_FILE)
-
-os.mkdirs(logs_path, exist_ok=True)
-
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-
+# Logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format = "[%(asctime)s ] %(lineno)d" %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+
+logger = logging.getLogger("my_agentic_app")
