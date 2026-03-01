5c8481f9185d91b1bba3710880a42a45b48c896c
nikhilsai064 <nikhilsai064@gmail.com>
2026-02-28 22:05:10 -0600

    aaa

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,6 +1,6 @@
 import yaml
 
-def load_config(config_path: str = r"config\\config.yaml") -> dict:
+def load_config(config_path: str = r"C:\Users\pachi\my learnings\2.0\6.GEN AI\projects\agentic_trading_bot\config\config.yaml") -> dict:
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
     return config
